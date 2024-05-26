e56390c540eaa40284e5a3472bc9482ebf8a0106
jiangpeng <11565373+bysomeone@users.noreply.github.com>
2024-05-26 23:23:34 +0800

    update test config

--- build/temp/build.sh
+++ build/temp/build.sh
@@ -4,15 +4,20 @@
 #declare -a arr=("docker" "para")
 declare -a arr=("para")
 
-chain33dir="/home/jiangpeng/go/src/github.com/33cn/chain33"
+chain33dir="/home/jiangpeng/go/src/github.com/33cn/plugin"
 srcdir="/home/jiangpeng/go/src/github.com/33cn/plugin"
 testdir="/home/jiangpeng/go/src/test"
 
 
 
-cd $chain33dir && make clean && make && cd -
+cd $chain33dir && make && cd -
 
 cp $chain33dir/build/chain33 ./ || echo "faild"
-#cp $chain33dir/build/chain33-cli /usr/local/bin/33cli
+cp $chain33dir/build/chain33-cli /usr/local/bin/33cli
 cp $chain33dir/build/chain33 ../temp1
 cp $chain33dir/build/chain33 ../temp2
+
+rm -rf *.log datadir logs
+cd ../temp1 && rm -rf *.log datadir logs && cd -
+cd ../temp2 && rm -rf *.log datadir logs && cd -
+
--- go.mod
+++ go.mod
@@ -2,7 +2,7 @@ module github.com/33cn/plugin
 
 go 1.19
 
-replace github.com/33cn/chain33 => github.com/bysomeone/chain33 v0.0.0-20240512025003-55d7dc44896a
+replace github.com/33cn/chain33 => ../chain33
 
 require (
 	github.com/33cn/chain33 v0.0.0-00010101000000-000000000000
@@ -268,7 +268,7 @@ require (
 )
 
 replace (
-	github.com/ava-labs/avalanchego => github.com/bysomeone/avalanchego v0.0.0-20240511070301-a9a2aac50464
+	github.com/ava-labs/avalanchego => ../../ava-labs/avalanchego
 	github.com/btcsuite/btcd/btcec => github.com/btcsuite/btcd v0.22.3
 	github.com/consensys/gnark-crypto => github.com/consensys/gnark-crypto v0.5.3
 )
